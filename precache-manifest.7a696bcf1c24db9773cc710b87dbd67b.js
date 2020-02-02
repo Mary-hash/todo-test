b97fb8052d1db5219b27dd97640798babeb46be0
Mary-hash <60557643+Mary-hash@users.noreply.github.com>
2020-02-02 22:45:43 +0200

    Add files via upload

---
+++ precache-manifest.7a696bcf1c24db9773cc710b87dbd67b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58abb1e4b8384a1090f56fe5ac3d3a70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e359337e564e3472707",
+    "url": "/static/css/main.5902b626.chunk.css"
+  },
+  {
+    "revision": "668833b65e34c6594b7a",
+    "url": "/static/js/2.8b4deb28.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.8b4deb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e359337e564e3472707",
+    "url": "/static/js/main.748cedcf.chunk.js"
+  },
+  {
+    "revision": "e55ae5b8043bd6237822",
+    "url": "/static/js/runtime-main.7befcefc.js"
+  }
+]);
